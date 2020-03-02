3d8462363cf86e5de3b58cd119ef2053bdbab557
Caroline Finnerty <s529428@nwmissouri.edu>
2020-03-01 22:42:36 -0600

    Enhanced the js I guess

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 $(document).ready(function(){
+    //Loads the basket from the previous session. 
+    $("#basket").text(localStorage.getItem("groceryList"));
+    //Loads the total amount from the previous session. 
+    $("#total_amount").text(localStorage.getItem("totalAmount"));
+    //Loads the tax amount from the previous session. 
+    $("#tax_amount").text(localStorage.getItem("taxAmount"));
+    //Calculation Function
     $("#submitButton").click(function(){
-        $("#basket").text( $("#basket").text() + $("input[name='itemName']").val() + ", ");
-        $("#total_amount").text(Number($("#total_amount").text()) + Number($("input[name='itemAmount']").val()));
-        $("#tax_amount").text(Number($("#total_amount").text()) * 0.25);
+        //Add to the basket
+        localStorage.setItem("groceryList", (localStorage.getItem("groceryList") + $("input[name='itemName']").val() + ", "));
+        $("#basket").text(localStorage.getItem("groceryList"));
+        //Add to the total
+        localStorage.setItem("totalAmount", (Number(localStorage.getItem("totalAmount")) + Number($("input[name='itemAmount']").val())));
+        $("#total_amount").text(localStorage.getItem("totalAmount"));
+        //Add the tax amount
+        localStorage.setItem("taxAmount", (Number($("#total_amount").text()) * 0.25));
+        $("#tax_amount").text(localStorage.getItem("taxAmount"));
+    });
+
+    $("#clearButton").click(function(){
+
     });
 });
+
 
